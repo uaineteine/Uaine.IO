95f68b77bec192e77e54270d33d4da52b8934752
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2021-08-04 16:08:13 +1000

    creates directory for filepath if it doesn't exist

--- FileData.cs
+++ FileData.cs
@@ -1,16 +1,39 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 
 namespace Uaine.IO
 {
     public class FileData
     {
+        protected string dir_in = "";
+        public string DirIn => dir_in;
         protected string Filename;
+        public string FullFilePath => Filename;
         public string[] Lines;
 
         public FileData(string filename)
         {
             Filename = filename;
+
+            //make if directory exists
+            string dir_in = "";
+            try
+            {
+                dir_in = Path.GetDirectoryName(filename);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            try
+            {
+                Directory.CreateDirectory(dir_in);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
 
         public void LoadAllLines()
