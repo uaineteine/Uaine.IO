500ade154ac974ba66b67a618540fbc82c048dd9
uaineteine <39141263+uaineteine@users.noreply.github.com>
2021-08-21 20:39:33 +1000

    Update checksum.cs
    properties to checksum class

--- Checksum/checksum.cs
+++ Checksum/checksum.cs
@@ -7,8 +7,11 @@ namespace Uaine.IO.Checksum
     public class checksum
     {
         private int type;
+        public int Type => type;
         private byte[] hash;
+        public byte[] ChecksumHash => hash;
         private string checkstring;
+        public string ChecksumString => checkstring;
 
         public checksum(int type, byte[] hash, string checkstring)
         {
